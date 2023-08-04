e3a75632c550a7f37315db4f71700107889bfcb1
arthurbright <55501753+arthurbright@users.noreply.github.com>
2023-08-04 00:04:35 -0400

    huntee update

--- index.js
+++ index.js
@@ -2,11 +2,31 @@ const express = require('express')
 const app = express()
 const path = require('path')
 
-app.use(express.static('public'))
+app.use(express.static('public'));
+app.use(express.json());
 
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, 'public/main.html'));
 });
+
+var hunteeLat = 0;
+var hunteeLon = 0;
+var hunteeName = "Gulf of Guinea (lat lon 0 0)";
+
+app.get('/huntee', function(req, res) {
+  var obj = {lat: hunteeLat, lon: hunteeLon, name: hunteeName};
+  res.send(obj);
+});
+
+app.post('/huntme', function(req, res) {
+  console.log(req.body);
+  hunteeLat = req.body.lat;
+  hunteeLon = req.body.lon;
+  hunteeName = req.body.name;
+  res.status(200);
+});
+
+
 
 const port = process.eventNames.PORT || 3000;
 
--- public/main.js
+++ public/main.js
@@ -1,6 +1,18 @@
+//
+
 var x = document.getElementById("textbox");
+var x2 = document.getElementById("textbox2");
+var namebox = document.getElementById("namebox");
+
+var dummyLat = 43.8915;
+var dummyLong = -79.40246;
+
+var lat = 0;
+var lon = 0;
 
 var numUpdates = 0;
+
+var hunted = false;
 
 function getLocation() {
     if (navigator.geolocation) {
@@ -27,10 +39,63 @@ function errorout(error){
     }
   }
     
-  function showPosition(position) {
+function showPosition(position) {
+    lat = position.coords.latitude;
+    lon = position.coords.longitude;
+
+    /*
     x.innerHTML = "Latitude: " + position.coords.latitude +
     "<br>Longitude: " + position.coords.longitude + 
     "<br>Altitude: " + position.coords.altitude + 
     "<br>Heading: " + position.coords.heading + 
-    "<br>Updates: " + numUpdates;
+    "<br>Updates: " + numUpdates;*/
+    numUpdates += 1;
+}
+
+////////////////////////////////////////////
+getLocation();
+setInterval(() => {
+    updateText()
+}, 5000);
+
+async function updateText(){
+    const url = "/huntee";
+    var data = await axios.get(url, {});
+    if(data.data.name == '') data.data.name = "Unnamed User";
+
+    x2.innerHTML = "Distance to " + data.data.name + ": " + getDistanceFromLatLonInKm(data.data.lat, data.data.lon, lat, lon) + " km";
+}
+
+
+function huntMe(){
+    const url = "/huntme";
+    const data = {lat: lat, lon: lon, name: namebox.value}
+    axios.post(url, data);
+}
+
+
+
+
+
+
+
+
+///////////////////////////////////////////////////////////////////////////////////util
+//https://stackoverflow.com/questions/18883601/function-to-calculate-distance-between-two-coordinates
+function getDistanceFromLatLonInKm(lat1, lon1, lat2, lon2) {
+  var R = 6378.1370; // Radius of the earth in km
+  var dLat = deg2rad(lat2-lat1);  // deg2rad below
+  var dLon = deg2rad(lon2-lon1); 
+  var a = 
+    Math.sin(dLat/2) * Math.sin(dLat/2) +
+    Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * 
+    Math.sin(dLon/2) * Math.sin(dLon/2)
+    ; 
+  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+  var d = R * c; // Distance in km
+  return d;
+}
+
+function deg2rad(deg) {
+  return deg * (Math.PI/180)
 }
